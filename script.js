07205f5feff08f60df3e043389be79a09133c3d4
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-25 19:41:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,27 +11,60 @@ const products = [
 
 // DOM elements
 const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerHTML = `${product.name} - $${product.price} 
+	<button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
     productList.appendChild(li);
   });
+
+	document.querySelectorAll(".add-to-cart-btn").forEach((button) => {
+		button.addEventListener("click", (event) => {
+			const productId = parseInt(event.target.dataset.id);
+			 addToCart(productId);
+ 		});
+	});
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+	cartList.innerHTML = "";
+	cart.forEach((item) => {
+		const li =document.createElement("li");
+		li.innerHTML = `${item.name} - $${item.price}`;
+		cartList.appendChild(li);
+	});
+	sessionStorage.setItem("cart", JSON.stringify(cart));
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	const product = products.find((p) => p.id === productId);
+  if (product) {
+    cart.push(product);
+    renderCart();
+  }
+}
 
 // Remove item from cart
-function removeFromCart(productId) {}
+function removeFromCart(productId) {
+	cart = cart.filter((item) => item.id !== productId);
+	renderCart();
+}
 
 // Clear cart
-function clearCart() {}
+function clearCart() {
+	cart = [];
+	renderCart();
+}
+clearCartBtn.addEventListener("click", clearCart);
 
 // Initial render
 renderProducts();
